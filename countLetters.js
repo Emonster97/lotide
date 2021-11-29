5a67eb850aa401964b4090dab5e4635a4125159f
Wilfred <wilfredemonts@gmail.com>
2021-11-29 06:32:46 -0500

    countLetters being added to Lotide

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let freq = {};
+  
+  for (let a of sentence) { 
+      freq[a] = (freq[a] !== undefined ? freq[a] + 1 : 1);
+  }
+  return freq;
+}
+
+console.log(countLetters("lighthouse in the house"));
